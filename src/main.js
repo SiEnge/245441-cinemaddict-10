39438066564d5b6ae0e6d028084ac3b24e59e6df
sienge <enge666-86@yandex.ru>
2020-01-04 20:21:26 +0300

    Вывод блока, если нет фильмов

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import MenuComponent from './components/menu.js';
 import SortComponent from './components/sort.js';
 import FilmsContainerComponent from './components/films-container.js';
 import FilmListComponent from './components/film-list.js';
+import NoFilmsListComponent from './components/film-list-no-data.js';
 import FilmListExtraComponent from './components/film-list-extra.js';
 import FilmComponent from './components/film.js';
 import PopupComponent from './components/popup.js';
@@ -52,14 +53,6 @@ const renderFilm = (film, place) => {
     filmPopupComponent.getElement().remove();
     // render(bodyElement, filmPopupComponent.getElement(), RenderPosition.BEFOREEND);
   };
-
-  // const replaceEditToTask = () => {
-  //   taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-  // };
-
-  // const replaceTaskToEdit = () => {
-  //   taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-  // };
 
   // создание новых компонент карточки и формы редактирования
   const filmComponent = new FilmComponent(film);
@@ -122,72 +115,77 @@ const filmsContainer = new FilmsContainerComponent();
 render(mainElement, filmsContainer.getElement(), RenderPosition.BEFOREEND);
 
 // если есть фильм (а пока они всегда есть), то отрисовать контейнер для фильмов и сами фильмы в него
-const filmList = new FilmListComponent();
-render(filmsContainer.getElement(), filmList.getElement(), RenderPosition.BEFOREEND);
+if (films.length === 0) {
+  render(filmsContainer.getElement(), new NoFilmsListComponent().getElement(), RenderPosition.BEFOREEND);
+} else {
+  const filmList = new FilmListComponent();
+  render(filmsContainer.getElement(), filmList.getElement(), RenderPosition.BEFOREEND);
 
-// 5. вставка "Карточки фильма"
-let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-const filmContainerElement = filmList.getElement().querySelector(`.films-list__container`);
+  // 5. вставка "Карточки фильма"
+  let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+  const filmContainerElement = filmList.getElement().querySelector(`.films-list__container`);
 
-// films.slice(0, showingFilmsCount).forEach((film) => render2(filmContainerElement, createFilmCardTemplate(film), `beforeend`));
-films.slice(0, showingFilmsCount)
-.forEach((film) => {
-  renderFilm(film, filmContainerElement);
-});
-
-// переписать на функции
-const extraTopRatedFilms = films.slice()
-  .sort((a, b) => b.rating - a.rating)
-  .filter((film) => film.rating !== 0)
-  .slice(0, 2);
-
-const extraMostCommentedFilms = films.slice()
-  .sort((a, b) => b.comments - a.comments)
-  .filter((film) => film.comments !== 0)
-  .slice(0, 2);
-
-
-// тут нужно делать проверку на наличие фильмов в массиве, если есть то отрисовывать
-
-const filmListExtraTopRated = new FilmListExtraComponent(`Top rated`);
-render(filmsContainer.getElement(), filmListExtraTopRated.getElement(), RenderPosition.BEFOREEND);
-extraTopRatedFilms.forEach((film) => {
-  renderFilm(film, filmListExtraTopRated.getElement().querySelector(`.films-list__container`));
-});
-// extraTopRatedFilms.forEach((film) => render(filmListExtraTopRated.getElement().querySelector(`.films-list__container`), new FilmComponent(film).getElement(), RenderPosition.BEFOREEND));
-
-const filmListExtraMostCommented = new FilmListExtraComponent(`Most commented`);
-render(filmsContainer.getElement(), filmListExtraMostCommented.getElement(), RenderPosition.BEFOREEND);
-extraMostCommentedFilms.forEach((film) => {
-  renderFilm(film, filmListExtraMostCommented.getElement().querySelector(`.films-list__container`));
-});
-// extraMostCommentedFilms.forEach((film) => render(filmListExtraMostCommented.getElement().querySelector(`.films-list__container`), new FilmComponent(film).getElement(), RenderPosition.BEFOREEND));
-
-
-// 6. вставка "Кнопки Показать еще"
-const showMoreButton = new ShowMoreButtonComponent();
-render(filmList.getElement(), showMoreButton.getElement(), `beforeend`);
-
-// вешаем обработчики на кнопку
-// обработка клика на кнопке загрузить еще
-showMoreButton.getElement().addEventListener(`click`, () => {
-  // записать в константу сколько было показано задач (=8)
-  const prevFilmsCount = showingFilmsCount;
-  // вычислить последний индекс карточки для показа (=16), чтобы применить в slice
-  showingFilmsCount = showingFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON;
-
-  // исходный массив с задачами скопировать (=slice) в количестве с 8 по 16
-  films.slice(prevFilmsCount, showingFilmsCount)
+  // films.slice(0, showingFilmsCount).forEach((film) => render2(filmContainerElement, createFilmCardTemplate(film), `beforeend`));
+  films.slice(0, showingFilmsCount)
   .forEach((film) => {
     renderFilm(film, filmContainerElement);
   });
 
-  // если показыны все задачи - то удалить кнопку
-  if (showingFilmsCount >= films.length) {
-    showMoreButton.getElement().remove();
-    showMoreButton.removeElement();
-  }
-});
+  // переписать на функции
+  const extraTopRatedFilms = films.slice()
+    .sort((a, b) => b.rating - a.rating)
+    .filter((film) => film.rating !== 0)
+    .slice(0, 2);
+
+  const extraMostCommentedFilms = films.slice()
+    .sort((a, b) => b.comments - a.comments)
+    .filter((film) => film.comments !== 0)
+    .slice(0, 2);
+
+
+  // тут нужно делать проверку на наличие фильмов в массиве, если есть то отрисовывать
+
+  const filmListExtraTopRated = new FilmListExtraComponent(`Top rated`);
+  render(filmsContainer.getElement(), filmListExtraTopRated.getElement(), RenderPosition.BEFOREEND);
+  extraTopRatedFilms.forEach((film) => {
+    renderFilm(film, filmListExtraTopRated.getElement().querySelector(`.films-list__container`));
+  });
+  // extraTopRatedFilms.forEach((film) => render(filmListExtraTopRated.getElement().querySelector(`.films-list__container`), new FilmComponent(film).getElement(), RenderPosition.BEFOREEND));
+
+  const filmListExtraMostCommented = new FilmListExtraComponent(`Most commented`);
+  render(filmsContainer.getElement(), filmListExtraMostCommented.getElement(), RenderPosition.BEFOREEND);
+  extraMostCommentedFilms.forEach((film) => {
+    renderFilm(film, filmListExtraMostCommented.getElement().querySelector(`.films-list__container`));
+  });
+  // extraMostCommentedFilms.forEach((film) => render(filmListExtraMostCommented.getElement().querySelector(`.films-list__container`), new FilmComponent(film).getElement(), RenderPosition.BEFOREEND));
+
+
+  // 6. вставка "Кнопки Показать еще"
+  const showMoreButton = new ShowMoreButtonComponent();
+  render(filmList.getElement(), showMoreButton.getElement(), `beforeend`);
+
+  // вешаем обработчики на кнопку
+  // обработка клика на кнопке загрузить еще
+  showMoreButton.getElement().addEventListener(`click`, () => {
+    // записать в константу сколько было показано задач (=8)
+    const prevFilmsCount = showingFilmsCount;
+    // вычислить последний индекс карточки для показа (=16), чтобы применить в slice
+    showingFilmsCount = showingFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON;
+
+    // исходный массив с задачами скопировать (=slice) в количестве с 8 по 16
+    films.slice(prevFilmsCount, showingFilmsCount)
+    .forEach((film) => {
+      renderFilm(film, filmContainerElement);
+    });
+
+    // если показыны все задачи - то удалить кнопку
+    if (showingFilmsCount >= films.length) {
+      showMoreButton.getElement().remove();
+      showMoreButton.removeElement();
+    }
+  });
+
+}
 
 // 8. вставка Комментарий
 // const comments = generateComments(COMMENT_COUNT);
